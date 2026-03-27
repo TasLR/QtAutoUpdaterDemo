5867d1c1ff94ba502aa4197dde8fb6b267743f4b
TasLR <fri_lan@163.com>
2026-03-27 18:39:07 +0800

    initial commit

---
+++ installer/setup.nsi
@@ -0,0 +1,19 @@
+!define APP_NAME "QtAutoUpdaterDemo"
+!define COMPANY "Demo"
+!define URL "https://github.com/你的用户名/QtAutoUpdaterDemo"
+
+SetCompressor /SOLID lzma
+Name "${APP_NAME}"
+OutFile "${APP_NAME}-Setup-${APP_VERSION}.exe"
+InstallDir "$PROGRAMFILES64\${APP_NAME}"
+
+!include "MUI2.nsh"
+!insertmacro MUI_PAGE_WELCOME
+!insertmacro MUI_PAGE_DIRECTORY
+!insertmacro MUI_PAGE_INSTFILES
+!insertmacro MUI_PAGE_FINISH
+
+SetOverwrite on
+File /r "${BUILD_DIR}/release/bin/*"
+
+CreateShortCut "$DESKTOP\${APP_NAME}.lnk" "$INSTDIR\${APP_NAME}.exe"
---
+++ src/mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include "QSimpleUpdater.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    setWindowTitle("QtAutoUpdaterDemo " APP_VERSION);
+    resize(400, 300);
+    checkForUpdates();
+}
+
+void MainWindow::checkForUpdates()
+{
+    // 这里改成你自己的仓库地址
+    const QString updateUrl = "https://raw.githubusercontent.com/你的用户名/QtAutoUpdaterDemo/main/update.json";
+
+    auto updater = QSimpleUpdater::getInstance();
+    updater->setModuleVersion("QtAutoUpdaterDemo", APP_VERSION);
+    updater->setNotifyOnUpdate("QtAutoUpdaterDemo", true);
+    updater->checkForUpdates("QtAutoUpdaterDemo", updateUrl);
+
+    qDebug() << "Update check done.";
+}
